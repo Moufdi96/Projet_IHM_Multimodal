26e898676b78a4f8c110a91684b280d7c5528e7e
moufdi96 <moufdi_taha@outlook.fr>
2020-11-26 01:34:58 +0100

    Reorganisation of dollarlib code into diffrent files

--- dollarlib/learnShape.py
+++ dollarlib/learnShape.py
@@ -1,37 +1,36 @@
-from tkinter import *
 from createCanvas import CreateCanvas 
-import json 
+from jsonFile_templates import* 
+ 
+#class LearnShape:
+#
+#    def __init__(self):
+#        
+#        self.templates = []
+#        self.stroke = 0
+#        self.listFigurePoints = []
+#        
+#        
+#    def loadJsonFile(self):
+#        jsonText = ''    
+#        with open("/home/moufdi/dollarlib/templates_file.json", "r") as templates_file:
+#            #templates = json.load(templates_file)
+#            jsonText = templates_file.read()
+#            self.templates = json.loads(jsonText)
 
-class LearnShape:
-
-    def __init__(self):
-        
-        self.templates = []
-        self.stroke = 0
-        self.listFigurePoints = []
-        self.color  ='red'
-        
-    def loadJsonFile(self):
-        jsonText = ''    
-        with open("/home/moufdi/dollarlib/templates_file.json", "r") as templates_file:
-            #templates = json.load(templates_file)
-            jsonText = templates_file.read()
-            self.templates = json.loads(jsonText)
-
-    def saveTemplate(self,templateName):
-        with open("/home/moufdi/dollarlib/templates_file.json", "w") as templates_file:
-            self.templates.append((self.listFigurePoints,templateName))
-            json.dump(self.templates,templates_file)
+#    def saveTemplate(self,templateName):
+#        with open("/home/moufdi/dollarlib/templates_file.json", "w") as templates_file:
+#            self.templates.append((self.listFigurePoints,templateName))
+#            json.dump(self.templates,templates_file)
 
 templateName = ''
 canvas = CreateCanvas()
-learnShape = LearnShape()
-
+ 
 while(templateName != 'stop'):
     templateName = input("enter the name\n")
-    learnShape.loadJsonFile()
+    templates = loadJsonFile()
     canvas.setCanvasWindow(600,400)
-    learnShape.saveTemplate(templateName)
+    listFigurePoints = canvas.listFigurePoints
+    saveTemplate(templates,listFigurePoints,templateName)
 
     
     
